d2dc2670554ac842c148bd47320dad0ed66e905f
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-18 19:28:18 -0500

    added navbar component

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            
+            <h1>Welcome to the final CareerDevs boss!</h1>
         </div>
     )
 }
