ea77ead99a5b12ebc2f495c110b3e19b8d40f568
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-23 18:19:38 -0500

    setting it up

--- apigateway/gateway.js
+++ apigateway/gateway.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes")
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use("/", routes)
 
 app.listen(PORT, () => {
     console.log("Gayway has started on pORT" + PORT);
