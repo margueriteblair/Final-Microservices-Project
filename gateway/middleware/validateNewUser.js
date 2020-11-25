ed10614eda57390ae6b9b3b8b917f16052b2b8a1
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 20:41:46 -0500

    validation middleware for a brand new user

--- gateway/middleware/validateNewUser.js
+++ gateway/middleware/validateNewUser.js
@@ -5,7 +5,13 @@ const bcrypt = require("bcrypt");
 
 module.exports = async (req, res, next) => {
     try {
+        const {email, username, password} = req.body;
+        const errorsList = [];
 
+        if (email === undefined || email.trim().length === 0) {
+            errorsList.push({key: "email", error: "email required"});
+        }
+        next();
     } catch (err) {
         res.status(500).json({message: err.message});
     }
