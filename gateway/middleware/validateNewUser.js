4261a1da41dde3d0cb7ea59d27197625fe809bde
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 20:50:13 -0500

    username validation

--- gateway/middleware/validateNewUser.js
+++ gateway/middleware/validateNewUser.js
@@ -8,8 +8,23 @@ module.exports = async (req, res, next) => {
         const {email, username, password} = req.body;
         const errorsList = [];
 
+
         if (email === undefined || email.trim().length === 0) {
-            errorsList.push({key: "email", error: "email required"});
+            errorsList.push({key: "email", error: "Email required."});
+        } else if (email !== undefined && !validator.isEmail(email)) {
+            errorsList.push({key: "email", error: "Must be a valid email address."});
+        } else {
+            const emailExist = await User.findOne({email: email}) !== null;
+            if (emailExist) errorsList.push({key: "email", error: "Email is already in use."})
+        }
+
+        if (username === undefined || username.trim().length == 0) {
+            errorsList.push({key: "username", error: "Username is required."});
+        } else {
+            const usernameExist = await User.findOne({username: username}) !== null;
+            if (usernameExist) {
+                errorsList.push({key: "username", error: "Username is already in use."})
+            }
         }
         next();
     } catch (err) {
