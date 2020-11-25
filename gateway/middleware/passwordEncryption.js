cf774b4c02f9aa6d1066c091c26285136d8ee29d
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 20:37:44 -0500

    completed the try catch block

--- gateway/middleware/passwordEncryption.js
+++ gateway/middleware/passwordEncryption.js
@@ -5,5 +5,10 @@ module.exports = async (req, res, next) => {
         const salt = await bcrypt.genSalt(10); //ten is the default either way
         const oldPassword = req.sanitized.password;
         const encryptedPassword = await bcrypt.hash(oldPassword, salt);
+        req.sanitized.password = encryptedPassword;
+        next();
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({message: error.message});
     }
 }
