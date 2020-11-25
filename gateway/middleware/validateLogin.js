44e49ef1f6e535aa1a03c4ede72c37aa9a69f49c
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 21:07:42 -0500

    found user

--- gateway/middleware/validateLogin.js
+++ gateway/middleware/validateLogin.js
@@ -8,7 +8,15 @@ module.exports = async (req, res, next) => {
         const {password, credential} = req.body; //for the login, the user will only provide a password and either their username or email
         const field = validator.isEmail(credential) ? "email" : "username";
 
-        const foundUser = await User.findOne({field})
+        const foundUser = await User.findOne({field: credential}, {password: 1});
+
+        if (foundUser === null) return res.status(400).json({message: "Email and/or password don't match."});
+
+        const passwordMatches = bcrypt.compare(password, foundUser.password);
+
+        if (!passwordMatches) return res.status(400).json({message: "Email and/or password don't match."})
+
+        req.userId = foundUser._id;
 
     } catch (err) {
         res.status(500).json({message: err.message});
