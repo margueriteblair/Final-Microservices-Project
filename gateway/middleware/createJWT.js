a1c23a4ea8f9b5823157b533dab601cca533f6fa
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 20:31:08 -0500

    created jwt middleware

---
+++ gateway/middleware/createJWT.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = (req, res, next) => {
+    try {
+        req.createdJWT = jwt.sign({_id: req.userId}, secret, {expiresIn: "4h"});
+        next();
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({message: error.message, error: error});
+    }
+}
