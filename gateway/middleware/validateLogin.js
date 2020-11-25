96e6bec89172185e4973df6fe84b98281c820c77
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 23:41:59 -0500

    still troubleshooting

--- gateway/middleware/validateLogin.js
+++ gateway/middleware/validateLogin.js
@@ -7,10 +7,10 @@ module.exports = async (req, res, next) => {
     try {
         const {password, credential} = req.body; //for the login, the user will only provide a password and either their username or email
         const field = validator.isEmail(credential) ? "email" : "username";
-        console.log(field);
+
         const query ={}
         query[field] = credential.trim();
-        console.log(query);
+
         const foundUser = await User.findOne(query, {password: 1});
         console.log(foundUser);
         if (foundUser === null) return res.status(400).json({message: "Email and/or password don't match. No user found."});
