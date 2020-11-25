98b693a444bcb52ca0478c22da24d8135dc7819c
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-24 22:36:09 -0500

    working out bugs in validate login

--- gateway/middleware/validateLogin.js
+++ gateway/middleware/validateLogin.js
@@ -7,16 +7,15 @@ module.exports = async (req, res, next) => {
     try {
         const {password, credential} = req.body; //for the login, the user will only provide a password and either their username or email
         const field = validator.isEmail(credential) ? "email" : "username";
-        console.log("field is good")
-
-        const foundUser = await User.findOne({field: credential}, {password: 1});
-
-        if (foundUser === null) return res.status(400).json({message: "Email and/or password don't match."});
-
+        console.log(field);
+        const query ={}
+        query[field] = credential.trim();
+        console.log(query);
+        const foundUser = await User.findOne(query, {password: 1});
+        console.log(foundUser);
+        if (foundUser === null) return res.status(400).json({message: "Email and/or password don't match. No user found."});
         const passwordMatches = bcrypt.compare(password, foundUser.password);
-
-        if (!passwordMatches) return res.status(400).json({message: "Email and/or password don't match."})
-
+        if (!passwordMatches) return res.status(400).json({message: "Email and/or password don't match. Password failure."})
         req.userId = foundUser._id;
 
         next();
