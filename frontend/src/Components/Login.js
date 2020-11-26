d3fbe87a5dc34eac945b81e1ce94965e5de9c203
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 22:15:37 -0500

    fleshing out the login portion

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Form from './Form'
+
+import {loginReq} from '../utils/userRequests';
+import {loginInputs} from '../utils/userInputs';
 
 export default function Login() {
+    const regLink = '/register';
     return (
         <div>
-            
+            <br></br>
+            <Form
+            id="loginForm"
+            inputs={loginInputs}
+            title="Login to Your Account!"
+            submitFunc={loginReq}
+            ></Form>
         </div>
     )
 }
