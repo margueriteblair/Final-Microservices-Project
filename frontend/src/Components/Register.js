13462d9d1e2d02a685bddca6fb6502a202f72d38
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 22:09:41 -0500

    register page

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import Form from './Form';
+import {regInputs} from '../utils/userInputs';
+import {regRequest} from '../utils/userRequests';
 
 export default function Register() {
+    const loginLink = '/login';
     return (
         <div>
-            
+            <h1>Create a New Account</h1>
+            <br></br>
+            <Form
+            id="registerForm"
+            title="Register"
+            inputs={regInputs}
+            submitFunction={regRequest}
+            ></Form>
+            <button text="Already have an Account? Login" onClick={()=>{window.location = loginLink}}></button>
         </div>
     )
 }
