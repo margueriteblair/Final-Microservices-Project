af1c1542cbb5ccfd0c2520bbc3569f2ae453354f
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 22:50:44 -0500

    pushing changes

--- frontend/src/Components/Form.js
+++ frontend/src/Components/Form.js
@@ -24,6 +24,7 @@ export default function Form(props) {
                     ></Input>
                 }
             </form>
+            <button text="Submit" onClick={buttonOnClick}></button>
         </div>
     )
 }
