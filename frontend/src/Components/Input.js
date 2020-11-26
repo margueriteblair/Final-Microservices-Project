de334ad924238d5a9f74d0c7d89639d5ae20e853
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 22:24:34 -0500

    input component

--- frontend/src/Components/Input.js
+++ frontend/src/Components/Input.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Input() {
+export default function Input(props) {
     return (
-        <input style={{width:150, height:30}}></input>
+        <input
+        id={props.id}
+        name={props.name}
+        onChange={props.onChange}
+        type={props.type}
+        placeholder={props.placeholder}
+        style={{width:300, height: 50}}
+        ></input>
     )
 }
