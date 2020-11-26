f16498e924a9def0c8b4c11fdb5b1ee2b7b26728
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-25 22:22:38 -0500

    finished login page

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -4,7 +4,7 @@ import Form from './Form'
 import {loginReq} from '../utils/userRequests';
 import {loginInputs} from '../utils/userInputs';
 
-export default function Login() {
+export default function Login(props) {
     const regLink = '/register';
     return (
         <div>
@@ -15,6 +15,17 @@ export default function Login() {
             title="Login to Your Account!"
             submitFunc={loginReq}
             ></Form>
+            {props.loggedIn ? (
+                <div>
+                    <p>Already signed in!</p>
+                    <button text="Return home" onClick={() => {window.location = '/'}}></button>
+                </div>
+            ) : (
+                <div>
+                    <p>No Account?</p>
+                    <button text="Register Button" onClick={() => {window.location=regLink}}></button>
+                </div>
+            )}
         </div>
     )
 }
