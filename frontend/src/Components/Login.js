53313bc53a014dabd0f3b8d0c219c21920e37c18
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-29 12:00:07 -0500

    please send help

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,14 +1,12 @@
 import React, {useState} from 'react'
 import Form from './Form'
 import Button from '.Button';
-import axios from 'axios';
-import {setLocalSession} from '../utils/Common';
 
 import {loginReq} from '../utils/userRequests';
 import {loginInputs} from '../utils/userInputs';
 
 export default function Login(props) {
-    const [loading, setLoading] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
     const regLink = '/register';
 
     return (
