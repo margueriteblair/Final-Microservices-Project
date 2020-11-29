e85e527172cb8e9c83145b785b4df10ee3546417
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-29 12:32:25 -0500

    local storage util

---
+++ frontend/src/utils/localStorage.js
@@ -0,0 +1,11 @@
+module.exports = {
+    get: (key, initial) => {
+        const savedData = JSON.parse(localStorage.getItem(key));
+        if (savedData !== null) return savedData;
+        if (initial instanceof Function) return initial();
+        return initial;
+    },
+    set: (key, value) => {
+        localStorage.setItem(key, value);
+    }
+}
