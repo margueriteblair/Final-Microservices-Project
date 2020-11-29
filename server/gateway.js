8f147f6c02689f4647b831efc143170795007a06
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-29 16:38:26 -0500

    cleanup because body parser is now built into express

--- server/gateway.js
+++ server/gateway.js
@@ -3,12 +3,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-//an API gateway is an entry point to other services
-//nodejs works well for api gateways even if your microservice architecture is developed in a different language
-
-//most of the microservices infrastructre need to handle authentication
-//putting shared logic like authentication to the API gateway can help you to keep your services smalland domain focused
-//there's both cookie and token based authentication
 const app = express();
 const routes = require("./routes")
 const userRouter = require("./routes/userRouter");
@@ -16,7 +10,6 @@ const PORT = process.env.PORT;
 const logger = require("morgan");
 const URI = process.env.MONGO;
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 if (typeof(URI) === "string") {
     mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => {
@@ -30,8 +23,6 @@ if (typeof(URI) === "string") {
 app.use(express.json());
 app.use(cors());
 app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
 
 app.use("/", routes)
 app.use("/user", userRouter);
