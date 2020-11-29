9fa6545b9ce34f44bc97b279e302b06453c6a57e
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-29 16:45:30 -0500

    auth js

---
+++ server/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router();
+const config = require("../config/default");
+const axios = require("axios");
+
+router.post("/", async (req, res) => {
+    let response;
+    try {
+        switch(req.body.action) {
+            case "postUser":
+                response = await handleUserPost(req.body.reqBody);
+                break;
+            case "loginUser":
+                response = await handleUserLogin(req.body.reqBody);
+                break;
+            case "handleProfilePost":
+                response = await handleUserLogin(
+                    req.body.reqBody,
+                    req.header("x-auth-token")
+                );
+                break;
+            default:
+                return res.status(404).json({errors: {action: "invalid action"}});
+        }
+    }
+})
