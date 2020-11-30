549ef8c24944e19f737b50f5f18781cf578a9cf7
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-29 20:25:39 -0500

    small tweaks

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -4,11 +4,17 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const auth = require("./routes/auth")
+const router = require("./routes/router");
+
+app.get("/", (req, res) => {
+    res.send("Okay, official API gateway")
+})
 
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", auth);
+// app.use(router);
 
 const PORT = process.env.PORT || 3398;
 app.listen(PORT, () => {
