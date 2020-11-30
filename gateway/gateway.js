8165b3a4580920500f15af98417926e0c688098c
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-29 20:23:21 -0500

    slimmer gateway

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -1,16 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 const cors = require("cors");
-const routes = require("./routes")
 const auth = require("./routes/auth")
 
 
 app.use(express.json());
 app.use(cors());
-app.use("/", routes)
+app.use("/api/auth", auth);
 
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log("Gateway has started on port " + port);
+const PORT = process.env.PORT || 3398;
+app.listen(PORT, () => {
+    console.log("Gateway has started on port " + PORT);
 })
