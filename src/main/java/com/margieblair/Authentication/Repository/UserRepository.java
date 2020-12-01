4efc1d39bb1922d04c10a3388813a7f92679b98e
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-30 19:19:25 -0500

    user service

--- src/main/java/com/margieblair/Authentication/Repository/UserRepository.java
+++ src/main/java/com/margieblair/Authentication/Repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.margieblair.Authentication.Repository;
 
 import com.margieblair.Authentication.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserByName(String fullName);
     User findUserById(Integer id);
---
+++ src/main/java/com/margieblair/Authentication/Services/UserService.java
@@ -0,0 +1,7 @@
+package com.margieblair.Authentication.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
