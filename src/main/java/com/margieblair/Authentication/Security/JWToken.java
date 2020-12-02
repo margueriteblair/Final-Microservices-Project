573062455fdd9fe9f47f3d0c82ca9bc7e040e542
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-01 20:35:46 -0500

    adding jwt dependency

--- src/main/java/com/margieblair/Authentication/Security/JWToken.java
+++ src/main/java/com/margieblair/Authentication/Security/JWToken.java
@@ -3,7 +3,7 @@ package com.margieblair.Authentication.Security;
 import com.margieblair.Authentication.Model.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
+import io.jsonwebtoken.Jwts;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,5 +24,7 @@ public class JWToken {
         claims.put("id", (Integer.toString(user.getId())));
         claims.put("username", user.getUsername());
         claims.put("fullname", user.getFullName());
+
+        String jsonWebToken = Jwts.builder();
     }
 }
