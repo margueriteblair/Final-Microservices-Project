5bffdfce27e681ade1c33e4a4e931f4d6d28f087
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-02 01:48:16 -0500

    starting fresh with a new user model and a login control with request mapping

---
+++ src/main/java/com/margieblair/LoginControl.java
@@ -0,0 +1,57 @@
+package com.margieblair;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+@RestController
+public class LoginControl {
+    //MARGIE REPLACE WITH DB
+    private final ArrayList<User> users = new ArrayList<>();
+
+
+    @Autowired
+    public LoginControl() {
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginControl.class, args);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody String name, String password, int id){
+        User user = new User(name, password, id);
+        System.out.println("new user: " + name + " " + password + " "+ id);
+
+        return user;
+    }
+
+    @GetMapping(path = "{id}")
+    public User getUserById(@PathVariable("id") int id){
+        System.out.println("in get" + id);
+        User thisUser = users.stream().filter(
+                user -> user.getId() == id
+        ).findFirst().orElse(null);
+
+        System.out.println(thisUser);
+
+        return thisUser;
+    }
+
+    @DeleteMapping
+    public User deleteUserById(int id){
+        return null;
+    }
+
+    @PutMapping
+    public User updateUserById(int id, User newUser){
+        return newUser;
+    }
+
+}
