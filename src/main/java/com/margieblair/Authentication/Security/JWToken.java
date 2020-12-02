3c63d7b8fff4ed41f64655f9d09a98ece19384fd
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-01 20:39:29 -0500

    json web token added

--- src/main/java/com/margieblair/Authentication/Security/JWToken.java
+++ src/main/java/com/margieblair/Authentication/Security/JWToken.java
@@ -1,9 +1,10 @@
 package com.margieblair.Authentication.Security;
 
 import com.margieblair.Authentication.Model.User;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,13 +12,14 @@ import java.util.Map;
 @Component
 public class JWToken {
     private static final int EXPIRATION_TIME_LIMIT = 500_000;
+    private static final String JWT_SECRET = "dkldck4kl2laxf467sdx";
 //    public static final String SIGN_UP_URLS = "/api/users/**";
-//    public static final String SECRET = "MSAJWTsKey";
 //    public static final String TOKEN_PREFIX = "Bearer ";
 //    public static final String HEADER_STRING = "Authorization";
     public String generateToken(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         Date now = new Date(System.currentTimeMillis());
+        String userId = Integer.toString(user.getId());
 
         Date expiration = new Date(now.getTime() + EXPIRATION_TIME_LIMIT);
         Map<String, Object> claims = new HashMap<>();
@@ -25,6 +27,13 @@ public class JWToken {
         claims.put("username", user.getUsername());
         claims.put("fullname", user.getFullName());
 
-        String jsonWebToken = Jwts.builder();
+        String jsonWebToken = Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.ES512, JWT_SECRET)
+                .compact();
+        return jsonWebToken;
     }
 }
