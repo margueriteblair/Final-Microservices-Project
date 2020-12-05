a1adc6906931fddfeec4ae35738293eceebeb4e2
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-04 21:51:16 -0500

    user constructor made

--- src/main/java/com/margieblair/model/User.java
+++ src/main/java/com/margieblair/model/User.java
@@ -1,16 +1,20 @@
 package com.margieblair.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
-
+import org.springframework.data.annotation.Id;
 
 public class User {
     private String firstName;
     private String lastName;
+    @Id
     private long id;
+    private String email;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
