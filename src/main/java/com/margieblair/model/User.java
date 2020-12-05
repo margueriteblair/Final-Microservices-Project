ac2fc91d90afbd3c569f3d5a90666b9c034ba3bc
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-04 21:19:10 -0500

    baje otra actualizaction

---
+++ src/main/java/com/margieblair/model/User.java
@@ -0,0 +1,16 @@
+package com.margieblair.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private long id;
+}
