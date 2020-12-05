12978603e4de18f7d6dc5ee166c7bb92b826defa
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-04 22:42:48 -0500

    testingin the application class

--- src/main/java/com/margieblair/AuthenticationMicroservicesApplication.java
+++ src/main/java/com/margieblair/AuthenticationMicroservicesApplication.java
@@ -1,13 +1,29 @@
 package com.margieblair;
 
+import com.margieblair.model.User;
+import com.margieblair.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthenticationMicroservicesApplication {
 
+	@Autowired
+	private UserRepository database;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationMicroservicesApplication.class, args);
 	}
+	@Override
+	public void run(String... args) throws Exception {
+		database.deleteAll();
+		database.save(new User("Margie", "Blair", "mblair@bostonk12.org"));
+		database.save(new User("Hello", "World", "java@careerdevs.com"));
 
-}
+		for (User user : database.findAll()) {
+			System.out.println(user);
+		}
+	}
+
+	}
