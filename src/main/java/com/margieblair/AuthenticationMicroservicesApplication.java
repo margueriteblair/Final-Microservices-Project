bcd37dcdb3fc8b15bbfe13dd5f84a15f5f0408d7
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-04 22:44:32 -0500

    implements command line runner

--- src/main/java/com/margieblair/AuthenticationMicroservicesApplication.java
+++ src/main/java/com/margieblair/AuthenticationMicroservicesApplication.java
@@ -3,11 +3,12 @@ package com.margieblair;
 import com.margieblair.model.User;
 import com.margieblair.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationMicroservicesApplication {
+public class AuthenticationMicroservicesApplication implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository database;
