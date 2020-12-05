a3c482422bc699058cf8d6d8684af834ed949356
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-04 22:32:46 -0500

    pushing changes

--- src/main/java/com/margieblair/repository/UserRepository.java
+++ src/main/java/com/margieblair/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    //MongoDB id by default is a string, that's why String is the second value
     public List<User> findByFirstName(String firstName);
     public List<User> findByLastName(String lastName);
     public User findByEmail(String email);
