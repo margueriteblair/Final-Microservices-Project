c2c3b220643ff5280362ba133c71f73ecacaa3ab
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-05 18:00:50 -0500

    myrunner class

---
+++ src/main/java/com/margieblair/MyRunner.java
@@ -0,0 +1,17 @@
+package com.margieblair;
+
+import com.margieblair.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(MyRunner.class
+    );
+
+    @Autowired
+    private UserRepository repository;
+}
