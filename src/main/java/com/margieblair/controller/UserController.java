698f6f4824781c1cccc4b445c253878f0a3bfbce
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-05 18:11:59 -0500

    delted command line runner

--- src/main/java/com/margieblair/controller/UserController.java
+++ src/main/java/com/margieblair/controller/UserController.java
@@ -4,9 +4,10 @@ package com.margieblair.controller;
 import com.margieblair.model.User;
 import com.margieblair.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="/api")
@@ -15,10 +16,10 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userService.findAll();
-    }
+//    @GetMapping
+//    public List<User> getAllUsers() {
+//        return userService.findAll();
+//    }
 
     @PostMapping("/newuser")
     public User createUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password) {
