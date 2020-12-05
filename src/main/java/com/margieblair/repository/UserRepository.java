89909b5a652578e576b1abf0a1ab9c4362fab101
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-05 00:17:42 -0500

    new changes

--- src/main/java/com/margieblair/repository/UserRepository.java
+++ src/main/java/com/margieblair/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.margieblair.repository;
 
 import com.margieblair.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "users", path="users")
 public interface UserRepository extends MongoRepository<User, String> {
     //MongoDB id by default is a string, that's why String is the second value
     public List<User> findByFirstName(String firstName);
