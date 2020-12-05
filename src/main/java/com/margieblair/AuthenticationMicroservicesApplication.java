50122720a3123d64cec8382019752cc76de7a7d4
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-05 01:33:35 -0500

    changes made and successfully connected to db

--- src/main/java/com/margieblair/AuthenticationMicroservicesApplication.java
+++ src/main/java/com/margieblair/AuthenticationMicroservicesApplication.java
@@ -1,15 +1,11 @@
 package com.margieblair;
 
-import com.margieblair.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthenticationMicroservicesApplication {
 
-	@Autowired
-	private UserRepository database;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationMicroservicesApplication.class, args);
