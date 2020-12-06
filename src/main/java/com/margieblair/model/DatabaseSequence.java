66b28fe374cc773857b07c3e300006518816cf4f
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-05 21:28:08 -0500

    getters and seetters for db seq

--- src/main/java/com/margieblair/model/DatabaseSequence.java
+++ src/main/java/com/margieblair/model/DatabaseSequence.java
@@ -10,7 +10,21 @@ public class DatabaseSequence {
     private String id;
     private long seq;
 
+    public DatabaseSequence() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public long getSeq() {
         return seq;
+    }
+
+    public void setSeq(long seq) {
+        this.seq = seq;
     }
 }
