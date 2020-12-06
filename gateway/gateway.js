178b7e18ce59a706fda003f114e9944324f17fe8
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-05 23:37:14 -0500

    comments

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -10,6 +10,8 @@ const auth = require("./routes/authorization")
 app.get("/", (req, res) => {
     res.send("Okay, official API gateway")
 })
+// gateway >> auth service
+// frontend >> gateway >> authservice
 
 app.use(express.json());
 app.use(cors());
