8d2689dfb1debb2c07590f5e402f50f635ea5c8d
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-11 17:29:56 -0500

    post request for processing js

--- gateway/routes/processing.js
+++ gateway/routes/processing.js
@@ -1,4 +1,29 @@
 const baseURL = process.env.PROCESSING_SERVER_BASE
 const {default: axios} = require("axios");
 const express = require("express");
+const { json } = require("body-parser");
 const router = express.Router();
+
+router.post("/", (req, res) => {
+    const {action, data} = req.body;
+    try {
+        switch(action) {
+            case "loadData":
+                res.json(await loadFinancialData())
+                break;
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(error);
+    }
+})
+
+loadFinancialData = async () => {
+    try {
+        let response = await axios.post(`${PROCESSING_SERVER_BASE}/load`);
+        return response.data;
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(error);
+    }
+}
