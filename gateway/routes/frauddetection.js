9030a0fee4c132a96b9f2de0980684de63be19e4
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-12 18:21:38 -0500

    fraud detection

--- gateway/routes/frauddetection.js
+++ gateway/routes/frauddetection.js
@@ -2,3 +2,17 @@ const baseURL = process.env.ML_SERVER_BASE
 const {default: axios} = require("axios");
 const express = require("express")
 const router = express.Router();
+
+router.post("/", async(req, res) => {
+    const {action, data} = req.body;
+    try {
+        switch(action) {
+            case "analyzeData":
+                res.json(await runFraudDetection());
+                break;
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json(error);
+    }
+})
